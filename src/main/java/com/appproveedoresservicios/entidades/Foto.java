07684c722806c16e1fb6905862df618b89af930b
Walter Crespín <walter.crespin49@gmail.com>
2022-11-09 21:46:50 -0300

    1er commit de verdad

--- src/main/java/com/appproveedoresservicios/entidades/Foto.java
+++ src/main/java/com/appproveedoresservicios/entidades/Foto.java
@@ -1,5 +1,23 @@
 package com.appproveedoresservicios.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
 public class Foto {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid",strategy ="uuid2")
+    private Long id;
     
+    private String nombre;
+    private String mime;
+    
+    @Column(length=5000000)
+    private byte[] contenido;
 }
