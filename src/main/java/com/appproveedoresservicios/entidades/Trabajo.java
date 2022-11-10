f1d8fde33ff7865f8ba1fb64387778079c5690c3
memocayar <mariaemiliamocayar@gmail.com>
2022-11-09 21:43:00 -0300

    Trabajo

--- src/main/java/com/appproveedoresservicios/entidades/Trabajo.java
+++ src/main/java/com/appproveedoresservicios/entidades/Trabajo.java
@@ -1,5 +1,34 @@
 package com.appproveedoresservicios.entidades;
 
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@Setter
+@Entity
+
 public class Trabajo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    
+    private Proveedor proveedor;
+    
+    private Cliente cliente; 
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate fechaInicio;
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate fechaFin;
+    
+    private boolean alta; 
     
 }
