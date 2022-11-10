8231d199339f27588e3242a1889203c44db3f44b
BenjaCardozo <90334282+BenjaCardozo@users.noreply.github.com>
2022-11-10 14:12:09 -0300

    Revert "M emilia mocayar"

--- src/main/java/com/appproveedoresservicios/entidades/Trabajo.java
+++ src/main/java/com/appproveedoresservicios/entidades/Trabajo.java
@@ -1,35 +1,5 @@
 package com.appproveedoresservicios.entidades;
 
-import java.time.LocalDate;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-@Data
-@Entity
-
 public class Trabajo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
-    
-    @OneToOne(fetch = FetchType.LAZY)
-    private Proveedor proveedor;
-    
-    @OneToOne(fetch = FetchType.LAZY)
-    private Cliente cliente; 
-    
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    private LocalDate fechaInicio;
-    
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    private LocalDate fechaFin;
-    
-    private boolean alta; 
     
 }
