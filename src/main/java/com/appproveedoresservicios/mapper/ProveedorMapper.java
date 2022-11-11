6b943bc0356b76fe5dadcfeb7f228b377398b9e7
Walter Crespín <walter.crespin49@gmail.com>
2022-11-11 14:34:13 -0300

    Termine el mapper falta agregar el servicio de foto

--- src/main/java/com/appproveedoresservicios/mapper/ProveedorMapper.java
+++ src/main/java/com/appproveedoresservicios/mapper/ProveedorMapper.java
@@ -3,15 +3,27 @@ package com.appproveedoresservicios.mapper;
 import com.appproveedoresservicios.dto.ProveedorRequest;
 import com.appproveedoresservicios.dto.ProveedorResponse;
 import com.appproveedoresservicios.entidades.Proveedor;
+import com.appproveedoresservicios.enums.Rol;
 
 public class ProveedorMapper {
     
-    public Proveedor map(ProveedorRequest proveedorReqest){
+    public Proveedor map(ProveedorRequest proveedorRequest){
         
         Proveedor proveedor = new Proveedor();
         
-        proveedor.setNombre(proveedorReqest.getNombre());
-        proveedor.setCorreo(proveedorReqest.getCorreo());
+        proveedor.setNombre(proveedorRequest.getNombre());
+        proveedor.setCorreo(proveedorRequest.getCorreo());
+        proveedor.setClave(proveedorRequest.getClave());
+        proveedor.setBarrio(proveedorRequest.getBarrio());
+        //LE PUSE NULL PORQUE FALTA HACER EL SERVICIO DE FOTO TODAVÍA
+        proveedor.setFoto(null);
+        proveedor.setAlta(true);
+        proveedor.setRol(Rol.PROVEEDOR);
+        
+        proveedor.setContacto(proveedorRequest.getContacto());
+        proveedor.setDescripcion(proveedorRequest.getDescripcion());
+        proveedor.setRubro(proveedorRequest.getRubro());
+        proveedor.setDisponibilidad(proveedorRequest.getDisponibilidad());
         
         return proveedor;
     }
@@ -19,6 +31,17 @@ public class ProveedorMapper {
     public ProveedorResponse map(Proveedor proveedor){
         
         ProveedorResponse response = new ProveedorResponse();
+        
+        response.setNombre(proveedor.getNombre());
+        response.setCorreo(proveedor.getCorreo());
+        response.setBarrio(proveedor.getBarrio());
+        response.setAlta(proveedor.getAlta());
+        response.setRol(proveedor.getRol());
+        
+        response.setContacto(proveedor.getContacto());
+        response.setDescripcion(proveedor.getDescripcion());
+        response.setRubro(proveedor.getRubro());
+        response.setDisponibilidad(proveedor.getDisponibilidad());
         
         return response;
     }
