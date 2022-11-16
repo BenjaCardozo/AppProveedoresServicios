582372f5f92d2247d0f6a30cbc854b2522acf7f3
EnzoManrique <manriqueenzo1024@gmail.com>
2022-11-16 19:45:19 -0300

    Servicio y controlador de cliente terminado

--- src/main/java/com/appproveedoresservicios/repositorios/ClienteRepositorio.java
+++ src/main/java/com/appproveedoresservicios/repositorios/ClienteRepositorio.java
@@ -1,10 +1,13 @@
 package com.appproveedoresservicios.repositorios;
 
 import com.appproveedoresservicios.entidades.Cliente;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepositorio extends JpaRepository<Cliente, Long>{
-    
+public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByBarrio(String barrio);
+
 }
--- src/main/java/com/appproveedoresservicios/servicios/ClienteServicio.java
+++ src/main/java/com/appproveedoresservicios/servicios/ClienteServicio.java
@@ -2,23 +2,28 @@ package com.appproveedoresservicios.servicios;
 
 import com.appproveedoresservicios.dto.ClienteRequest;
 import com.appproveedoresservicios.dto.ClienteResponse;
+import com.appproveedoresservicios.dto.ListClienteResponse;
 import com.appproveedoresservicios.entidades.Cliente;
 import com.appproveedoresservicios.excepciones.ResourceNotFoundException;
 
 public interface ClienteServicio {
-    
+
     ClienteResponse crearCliente(ClienteRequest request);
-    
-    ClienteResponse modificarCliente(ClienteRequest request, Long id) throws Exception ;
-    
+
+    ClienteResponse modificarCliente(ClienteRequest request, Long id) throws Exception;
+
     Cliente findById(Long id) throws ResourceNotFoundException;
-    
+
     ClienteResponse findClienteById(Long id) throws ResourceNotFoundException;
-    
+
     Cliente eliminarCliente(Long id) throws Exception;
-    
-    void darBajaCliente(Long id) throws Exception ;
-    
-    void darAltaCliente(Long id) throws Exception ;
+
+    void darBajaCliente(Long id) throws Exception;
+
+    void darAltaCliente(Long id) throws Exception;
+
+    ListClienteResponse buscarClientePorBarrio(String barrio) throws ResourceNotFoundException;
+
+    ListClienteResponse listarClientes();
     
 }
