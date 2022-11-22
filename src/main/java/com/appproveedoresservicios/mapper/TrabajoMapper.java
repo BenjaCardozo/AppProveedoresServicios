648fcf1cfdfcf4a63c927cc50540945ae46b36ea
BenjaCardozo <benjamin.cardozo0@gmail.com>
2022-11-22 16:58:43 -0300

    trabajo mapper

--- src/main/java/com/appproveedoresservicios/mapper/TrabajoMapper.java
+++ src/main/java/com/appproveedoresservicios/mapper/TrabajoMapper.java
@@ -11,44 +11,44 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TrabajoMapper {
-    
+
     @Autowired
     TrabajoServicioImp trabajoServicioImp;
-    
-    public Trabajo map(TrabajoRequest trabajoRequest){
-        
+
+    public Trabajo map(TrabajoRequest trabajoRequest) {
+
         Trabajo trabajo = new Trabajo();
-        
+
         trabajo.setProveedor(trabajoRequest.getProveedor());
         trabajo.setCliente(trabajoRequest.getCliente());
         trabajo.setFechaInicio(trabajoRequest.getFechaInicio());
         trabajo.setFechaFin(trabajoRequest.getFechaFin());
-        
+
         return trabajo;
     }
-    
-    public TrabajoResponse map(Trabajo trabajo){
-        
+
+    public TrabajoResponse map(Trabajo trabajo) {
+
         TrabajoResponse response = new TrabajoResponse();
-        
+
         response.setId(trabajo.getId());
         response.setProveedor(trabajo.getProveedor());
         response.setCliente(trabajo.getCliente());
         response.setFechaInicio(trabajo.getFechaInicio());
         response.setFechaFin(trabajo.getFechaFin());
-        
+
         return response;
     }
-    
-    public List<TrabajoResponse> map (List<Trabajo> trabajos){
-        
-        List<TrabajoResponse> listResponse = new  ArrayList<>();
-        
+
+    public List<TrabajoResponse> map(List<Trabajo> trabajos) {
+
+        List<TrabajoResponse> listResponse = new ArrayList<>();
+
         for (Trabajo trabajo : trabajos) {
             listResponse.add(map(trabajo));
         }
-        
+
         return listResponse;
-    }    
-    
+    }
+
 }
