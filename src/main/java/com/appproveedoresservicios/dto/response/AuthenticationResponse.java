c3ad0adefa3379e852c516db7ef4accf8711384c
WalterC49 <walter.crespin49@gmail.com>
2022-11-27 22:18:05 -0300

    Cambie la devolucion del login

--- src/main/java/com/appproveedoresservicios/dto/response/AuthenticationResponse.java
+++ src/main/java/com/appproveedoresservicios/dto/response/AuthenticationResponse.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class AuthenticationResponse {
     
-    private String correo;
+    private Long id;
+    private String rol;
     private String token;
 }
