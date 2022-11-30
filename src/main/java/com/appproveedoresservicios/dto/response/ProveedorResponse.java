a65ad08b4a4a72e7f79bd2e5e3f44b572f4bc4dc
BenjaCardozo <benjamin.cardozo0@gmail.com>
2022-11-30 20:11:50 -0300

    .

--- src/main/java/com/appproveedoresservicios/dto/response/ProveedorResponse.java
+++ src/main/java/com/appproveedoresservicios/dto/response/ProveedorResponse.java
@@ -3,3 +3,19 @@ package com.appproveedoresservicios.dto.response;
 import com.appproveedoresservicios.enums.Rol;
 import lombok.Data;
 
+@Data
+public class ProveedorResponse {
+    
+    private Long id;
+    private String nombre;
+    private String correo;
+    private String barrio;
+    private Boolean alta;
+    private Rol rol; 
+    
+    private String contacto;
+    private String descripcion;
+    private String rubro;
+    private Double promedioFeedback;
+    private String disponibilidad;
+}
